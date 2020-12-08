b9c67e29c50f1885ce9fa8bb0bb485f5b1f7eb9d
AlexiosR <alexey.ratov@gmail.com>
2020-12-08 19:19:11 +0200

    08.12.2020

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict"
+let first = `
+<div>
+    <div class="container--picture__default">
+    <img src="" alt="heremustbepicture">
+    </div>
+    <div class="container--text">
+    <h4>
+        Lorem ipsum dolor sit.
+    </h4>
+    <p>
+        Lorem ipsum dolor sit amet consectetur.
+    </p>
+    </div>
+</div> `
+for(let i = 0;l <= 10;i++ ) {
+    first += i
+}
