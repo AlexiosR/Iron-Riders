8e9aa93382a298259c2eb4d6cdf48be6a3b99e94
AlexiosR <alexey.ratov@gmail.com>
2020-12-13 19:07:23 +0200

    13/12/2020

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 "use strict"
-let first = `
-<div>
-    <div class="container--picture__default">
-    <img src="" alt="heremustbepicture">
+for (let i = 1; i < 21; i++) {
+    let src = i + '.jpg';
+    document.getElementById('contant').innerHTML += `
+    <div class="container--js">
+        <div class="container--picture__default">
+            <img src="https://picsum.photos/142/100" alt="p">
+        </div>
+        <div class="container--text">
+            <h4>
+                Lorem ipsum dolor sit.
+            </h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur.
+            </p>
+        </div>
     </div>
-    <div class="container--text">
-    <h4>
-        Lorem ipsum dolor sit.
-    </h4>
-    <p>
-        Lorem ipsum dolor sit amet consectetur.
-    </p>
-    </div>
-</div> `
-for(let i = 0;l <= 10;i++ ) {
-    first += i
+    `
 }
