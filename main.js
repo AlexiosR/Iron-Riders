9838ec2b5639ff59e332336c1b0b490637cbe4e1
dmitrolov <dmitrolov@gmail.com>
2020-12-17 22:33:51 +0200

    bg fix, menu centered

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 "use strict"
 let cont = document.getElementById('contant');
+
 function selectseleOption(select) {
     let seleOption = Number(select);
     let result = "";
     
     for(let i = 1; i <= seleOption; i++){
         result += `
-        <div class="container--js">
-        <div class="container--picture__default">
-        <img class="randomPhotos"src="https://picsum.photos/142/100" alt="p">
-        </div>
-        <div class="container--text">
-        <h4>
-        Lorem ipsum dolor sit.
-        </h4>
-        <p>
-        Lorem ipsum dolor sit amet consectetur.
-        </p>
-        </div>
-        </div>`;
+            <div class="container--js">
+                <div class="container--picture__default">
+                    <img class="randomPhotos"src="https://picsum.photos/142/100" alt="p">
+                </div>
+                <div class="container--text">
+                    <h4>Lorem ipsum dolor sit.</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+            </div>
+        `;
+
     }
     cont.innerHTML = result;
 };
